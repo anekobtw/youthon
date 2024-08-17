e3e6c917d6031c453ed83c24e40216799149cf08
anekobtw <krolexonder@gmail.com>
2024-08-17 12:26:49 +0400

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""A lightweight YouTube data scraper."""
+
+__description__ = "A lightweight YouTube data scraper."
+__url__ = "https://github.com/anekobtw/youthon"
+
+__title__ = "youthon"
+__author__ = "anekobtw"
+__license__ = "MIT"
+__version__ = "0.1.0"
+
+
+from src.youthon import Channel
+
+__all__ = ("Channel")
---
+++ src/tests.py
@@ -0,0 +1,18 @@
+import Channel
+
+channel = Channel("https://www.youtube.com/@PewDiePie")
+
+print(channel.name)
+print(channel.description)
+print(channel.channel_url)
+print(channel.profile_photo_url)
+print(channel.channel_id)
+print(channel.subscribers_count)
+print(channel.video_count)
+
+print(channel.videos_page)
+print(channel.shorts_page)
+print(channel.community_page)
+print(channel.featured_channels_page)
+print(channel.playlists_page)
+print(channel.about_page)
