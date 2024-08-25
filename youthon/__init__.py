9e2f3c398ff77379678493b74032cbef0e443c96
anekobtw <krolexonder@gmail.com>
2024-08-25 20:25:52 +0400

    0.3.2

--- youthon/__init__.py
+++ youthon/__init__.py
@@ -6,9 +6,10 @@ __url__ = "https://github.com/anekobtw/youthon"
 __title__ = "youthon"
 __author__ = "anekobtw"
 __license__ = "MIT"
-__version__ = "0.2.1"
+__version__ = "0.3.2"
 
 from channel import Channel
 from video import Video
+from playlist import Playlist
 
-__all__ = ("Channel", "Video")
+__all__ = ("Channel", "Video", "Playlist")
